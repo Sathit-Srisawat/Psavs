77c402226a2404778ad7f19d3e598f4b32c62072
Sathit Srisawat <sathit@Sathits-MacBook-Air.local>
2020-11-12 10:55:06 +0700

    6

--- screens/Exercise.js
+++ screens/Exercise.js
@@ -1,16 +1,71 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button, TouchableOpacity, TextInput, FlatList, YellowBox, Alert } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+  TouchableOpacity,
+  Dimensions,
+  navigation,
+  FlatList,
+  Image
+} from 'react-native';
 
-export default class TimeTable extends React.Component {
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from "react-native-chart-kit";
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+Icon.loadFont();
+export default class Exercise extends React.Component {
+
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 0.4 }}>
+          <Image style={{ height: 300, width: Dimensions.get('window').width }} source={require('../img/3.jpg')} />
+          <View style={{ position: 'absolute', top: 160, left: 20, right: 20, bottom: 20 }}>
+            <Text style={{ fontSize: 45, fontWeight: '800', color: '#fff' }}>
+              Suggest Exercise For You
+            </Text>
+          </View>
+          <View style={styles.cards}>
+            <View style = {{margin : 10}}>
+              <Text>
+                Exercise
+              </Text>
+            </View>
+          </View>
 
+        </View>
       </View>
     );
   }
-}
+};
+
 const styles = StyleSheet.create({
-  
+
+  cards: {
+    alignSelf: "center",
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    elevation: 10,
+    position: 'absolute',
+    top: 280,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 });
--- screens/Food.js
+++ screens/Food.js
@@ -10,7 +10,8 @@ import {
   TouchableOpacity,
   Dimensions,
   navigation,
-  FlatList
+  FlatList,
+  Image
 } from 'react-native';
 
 import {
@@ -30,9 +31,41 @@ export default class Food extends React.Component {
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 0.4 }}>
+          <Image style={{ height: 300, width: Dimensions.get('window').width }} source={require('../img/2.jpg')} />
+          <View style={{ position: 'absolute', top: 160, left: 20, right: 0, bottom: 20 }}>
+            <Text style={{ fontSize: 50, fontWeight: '800', color: '#fff' }}>
+              Suggest Food For You
+            </Text>
+          </View>
+          <View style={styles.cards}>
+            <View style = {{margin : 10}}>
+              <Text>
+                Breakfast
+              </Text>
+            </View>
+          </View>
 
+        </View>
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+
+  cards: {
+    alignSelf: "center",
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    elevation: 10,
+    position: 'absolute',
+    top: 280,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
